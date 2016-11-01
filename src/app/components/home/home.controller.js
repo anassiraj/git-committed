10d8956d5a98d489891941cee670c83cc625f835
Rusu, Derek (dr513h) <dr513h@us.att.com>
2016-11-01 09:18:42 -0500

    minor update

--- src/app/components/home/home.controller.js
+++ src/app/components/home/home.controller.js
@@ -1,16 +1,5 @@
 export default class HomeController {
-	constructor(firebaseServices, $q) {
-
-		/*Testing firebaseServices*/
-		var temp = {'name' : 'James'};
-		firebaseServices.setData('/', temp);
-
-		var $promise = firebaseServices.getData('/');
-
-		$promise.then(function(data) {
-			console.log(data);
-		});
-		/*Delete if needed*/
+	constructor(firebaseServices) {
 
 	}
 }
