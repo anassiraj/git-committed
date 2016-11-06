8b451794e0fb43621b34978b45b1a3376d6bb5bf
Anas Siraj <anas.siraj@live.com>
2016-11-06 08:18:10 -0600

    data styling

--- src/app/components/home/floorhome/data/data.controller.js
+++ src/app/components/home/floorhome/data/data.controller.js
@@ -2,7 +2,22 @@ export default class DataController {
 
 	constructor($state, $stateParams, firebaseServices, $q, $scope) {
 
-	
+		var bar1 = document.getElementById('bar1'), L = 5;
+		for ( var b, h, i=0; i<L; i++ ) {
+		    h = 20 + parseInt(Math.random() * 80);
+		    b = document.createElement('div');
+		    b.style.height = h + 'px';
+		    bar1.appendChild(b);
+		}
+
+		var bar2 = document.getElementById('bar2'), L = 5;
+		for ( var b, h, i=0; i<L; i++ ) {
+		    h = 20 + parseInt(Math.random() * 80);
+		    b = document.createElement('div');
+		    b.style.height = h + 'px';
+		    bar2.appendChild(b);
+		}
+		
 	}
 	
 }
